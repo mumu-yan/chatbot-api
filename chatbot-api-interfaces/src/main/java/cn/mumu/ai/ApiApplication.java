d9c56703130a6432b1d662569d128f4da5f1a335
yanhz <2368608484@qq.com>
2024-06-20 22:30:59 +0800

    chatbot-api feat: init project

---
+++ chatbot-api-interfaces/src/main/java/cn/mumu/ai/ApiApplication.java
@@ -0,0 +1,16 @@
+package cn.mumu.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: mumu
+ * @Date: 2020/12/25 16:26
+ * @Description: 启动入口
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
